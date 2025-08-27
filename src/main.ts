a087883b77b253c76134a320893d8661ba10cf6b
Jan Poonthong <janpoonthong628@gmail.com>
2025-08-27 13:41:20 +0700

    fix(cors): update CORS origin to use environment variable for improved flexibility

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'https://orchestronic-frontend.vercel.app',
+    origin: process.env.FRONTEND_URL,
     credentials: true, // allow cookies
   });
 
