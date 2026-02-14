01f59668cd94e5225fd55b2544c940898cfd0adb
HtetAungShine6 <htetaungshine211299@gmail.com>
2026-02-14 19:39:07 +0700

    fix: bugs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Redirect, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { AppService } from './app.service';
 
@@ -9,6 +9,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Redirect('/api/docs', 302)
+  redirectToDocs() {
+    return;
+  }
+
+  @Get('health')
   getConnection() {
     return this.appService.getConnection();
   }
